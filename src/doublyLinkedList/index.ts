70cdae2bd4b11f581b773b931a8802da1ada0295
Rahul Rao <63695122+rahulrao0209@users.noreply.github.com>
2023-03-12 11:42:22 +0530

    Adding initial version of the Doubly linked list
    The commit adds an initial version of the Doubly linked list data structure with some standard methods. The methods being added are
    1. pushAll - appends an array of values as nodes on the linked list.
    2. push - appends a node/item to the linked list.
    3. getters for head, tail and length of the linked list.

--- src/doublyLinkedList/index.ts
+++ src/doublyLinkedList/index.ts
@@ -1,3 +1,78 @@
-class DoublyLinkedList {}
+class ListNode<T> {
+  value: T;
+  next: ListNode<T> | null;
+  prev: ListNode<T> | null;
+
+  constructor(value: T) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList<T> {
+  #head: ListNode<T> | null;
+  #tail: ListNode<T> | null;
+  #length: number;
+
+  constructor() {
+    this.#head = null;
+    this.#tail = null;
+    this.#length = 0;
+  }
+
+  /**
+   * Appends a list of items/nodes to the list.
+   * @param {T[]} values - array of values of the nodes to be appended to the list.
+   * @returns {DoublyLinkedList<T>} the updated list.
+   */
+  pushAll(values: T[]): DoublyLinkedList<T> {
+    if (!values.length) return this;
+    values.forEach((value) => this.push(value));
+    return this;
+  }
+
+  /**
+   * Appends an item/node to the list.
+   * @param {T} value - the value of the node
+   * @returns {DoublyLinkedList<T>} the updated list.
+   */
+  push(value: T): DoublyLinkedList<T> {
+    const listNode = new ListNode(value);
+
+    if (!this.#head) {
+      this.#head = listNode;
+      this.#tail = this.#head;
+    } else if (this.#tail) {
+      this.#tail.next = listNode;
+      listNode.prev = this.#tail;
+      this.#tail = listNode;
+    }
+
+    this.#length += 1;
+    return this;
+  }
+
+  /**
+   * @returns {ListNode<T> | null} the head of the linked list.
+   */
+  get head(): ListNode<T> | null {
+    return this.#head;
+  }
+
+  /**
+   * @returns {ListNode<T> | null} the tail of the linked list.
+   */
+  get tail(): ListNode<T> | null {
+    return this.#tail;
+  }
+
+  /**
+   * @returns {number} length of the linked list.
+   */
+  get length(): number {
+    return this.#length;
+  }
+}
 
 export default DoublyLinkedList;
