b449b70d73339fe227a710b3817e34b5ba56e5c5
Rahul Rao <63695122+rahulrao0209@users.noreply.github.com>
2023-03-25 14:49:35 +0530

    ds-ts | Completing test cases for queue data structure implementation
    The test cases for queue implementation have been completed via this commit which consists of tests covering dequeue, peek, toArray methods and also tests a queue initialized with a capacity value.

--- src/queue/index.test.ts
+++ src/queue/index.test.ts
@@ -82,4 +82,107 @@ describe("Queue", () => {
     expect(dequeuedValue).toBe(values[0]);
     expect(queue.size).toBe(initialSize + values.length - 1);
   });
+
+  test("dequeue method returns undefined if the queue is empty", () => {
+    if (!queue) return;
+    expect(queue.size).toBe(0);
+    expect(queue.isEmpty).toBe(true);
+
+    /**
+     * dequeue method will return undefined if the queue is empty.
+     */
+    expect(queue.dequeue()).toBe(undefined);
+  });
+
+  test("peek method returns the value at the front/start of the queue without removing it", () => {
+    if (!queue) return;
+    const initialSize = queue.size;
+    expect(initialSize).toBe(0);
+    expect(queue.isEmpty).toBe(true);
+
+    /**
+     * Add values to the queue using the enqueueAll method.
+     */
+    queue.enqueueAll(values);
+    expect(queue.size).toBe(initialSize + values.length);
+    expect(queue.isEmpty).toBe(false);
+
+    /**
+     * Use peek method to get the value at the front/start of the queue
+     * without removing it.
+     */
+    expect(queue.peek()).toBe(values[0]);
+    expect(queue.size).toBe(initialSize + values.length);
+  });
+
+  test("peek method returns undefined if the queue is empty", () => {
+    if (!queue) return;
+    expect(queue.size).toBe(0);
+    expect(queue.isEmpty).toBe(true);
+
+    /**
+     * Using peek method on an empty queue will return undefined.
+     */
+    expect(queue.peek()).toBe(undefined);
+  });
+
+  test("toArray method returns a list/array of all the values in the queue", () => {
+    if (!queue) return;
+    const initialSize = queue.size;
+    expect(initialSize).toBe(0);
+    expect(queue.isEmpty).toBe(true);
+
+    /**
+     * Add values to the queue using the enqueueAll method.
+     */
+    queue.enqueueAll(values);
+    expect(queue.size).toBe(initialSize + values.length);
+    expect(queue.isEmpty).toBe(false);
+
+    /**
+     * toArray will return a list of all the values in the queue.
+     */
+    expect(queue.toArray()).toEqual(values);
+  });
+
+  test("toArray method returns an empty array/list if the queue is empty", () => {
+    if (!queue) return;
+    expect(queue.size).toBe(0);
+    expect(queue.isEmpty).toBe(true);
+
+    /**
+     * Use toArray on an empty queue
+     */
+    expect(queue.toArray()).toEqual([]);
+  });
+
+  test("queue with a capacity provided, throws error if the size exceeds capacity", () => {
+    const capacity = 2;
+    queue = new Queue<Company>(2);
+
+    /**
+     * Initially the queue is empty and has a capacity of 2.
+     */
+    expect(queue.size).toBe(0);
+    expect(queue.isEmpty).toBe(true);
+    expect(queue.isFull).toBe(false);
+    expect(queue.capacity).toBe(capacity);
+
+    /**
+     * enqueue two nodes using enqueueAll method.
+     */
+    const subArray = values.slice(0, 2);
+    queue.enqueueAll(subArray);
+    expect(queue.size).toBe(subArray.length);
+    expect(queue.isEmpty).toBe(false);
+    expect(queue.isFull).toBe(true);
+
+    /**
+     * As the queue is full now, pushing another element
+     * should throw a capacity exceeded error.
+     */
+    expect(
+      () => queue && queue.enqueue({ name: "Apple", foundingYear: "1976" })
+    ).toThrowError("The queue is full. Cannot insert more nodes.");
+  });
 });
