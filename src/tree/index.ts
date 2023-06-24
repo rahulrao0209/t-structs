345d3573f726e2b65f59778b76e536d32fcc4ccb
Rahul Rao <63695122+rahulrao0209@users.noreply.github.com>
2023-06-24 14:44:16 +0530

    ds-ts | Adding tree traversal methods
    The commit adds the following tree traversals
    1. PreOrder traversal.
    2. InOrder traversal.
    3. PostOrder traversal.
    4. LevelOrderTraveral (Breadth first traversal)

--- src/tree/index.ts
+++ src/tree/index.ts
@@ -5,6 +5,10 @@ import {
   defaultEquals,
 } from "../utils";
 
+import Queue from "../queue";
+
+type Traversal = "PRE_ORDER" | "IN_ORDER" | "POST_ORDER" | "LEVEL_ORDER";
+
 export class TreeNode<T> {
   value: T;
   left: TreeNode<T> | null;
@@ -34,55 +38,135 @@ export abstract class Tree<T> {
     initialValues.length && this.insertAll(initialValues);
   }
 
+  /**
+   * Inserts multiple values into the tree.
+   * @param {T[]} values
+   */
   abstract insertAll(values: T[]): Tree<T>;
 
+  /**
+   * Inserts a value into the tree.
+   * @param {T} value
+   */
   abstract insert(value: T): Tree<T>;
 
-  protected preOrderTraversal(node: TreeNode<T> | null) {
-    /**
-     * Preorder traversal involves, visting the root node first
-     * followed by the left subtree and the right subtree at the end.
-     * (root, left, right) order.
-     */
-    if (!node) return;
-    console.log(node.value);
-    this.preOrderTraversal(node.left);
-    this.preOrderTraversal(node.right);
+  /**
+   * Preorder traversal involves, visting the root node first,
+   * followed by the left subtree and the right subtree at the end.
+   * (root, left, right) order.
+   * @param {TreeNode<T> | null} node
+   * @returns {T[]}
+   */
+  protected preOrderTraversal(node: TreeNode<T> | null): T[] {
+    const values: T[] = [];
+
+    const preOrderHelper = function (
+      node: TreeNode<T> | null,
+      values: T[]
+    ): T[] {
+      if (!node) return values;
+      values.push(node.value);
+      preOrderHelper(node.left, values);
+      preOrderHelper(node.right, values);
+
+      return values;
+    };
+
+    return preOrderHelper(node, values);
   }
 
-  protected inOrderTraversal(node: TreeNode<T> | null) {
-    /**
-     * Inorder traversal involves, visiting the left subtree first,
-     * followed by the root and then the right subtree at the end.
-     * (left, root, right) order.
-     */
-    if (!node) return;
-    this.inOrderTraversal(node.left);
-    console.log(node.value);
-    this.inOrderTraversal(node.right);
+  /**
+   * Inorder traversal involves, visiting the left subtree first,
+   * followed by the root and then the right subtree at the end.
+   * (left, root, right) order.
+   * @param {TreeNode<T> | null} node
+   * @returns {T[]}
+   */
+  protected inOrderTraversal(node: TreeNode<T> | null): T[] {
+    const values: T[] = [];
+    const inOrderHelper = function (
+      node: TreeNode<T> | null,
+      values: T[]
+    ): T[] {
+      if (!node) return values;
+      inOrderHelper(node.left, values);
+      values.push(node.value);
+      inOrderHelper(node.right, values);
+
+      return values;
+    };
+
+    return inOrderHelper(node, values);
   }
 
-  protected postOrderTraversal(node: TreeNode<T> | null): void {
-    /**
-     * Postorder traversal involves visiting the left
-     * subtree, followed by the right subtree and then
-     * visiting the root node at last.
-     * (left, right, root) order
-     */
-    if (!node) return;
-    this.postOrderTraversal(node.left);
-    this.postOrderTraversal(node.right);
-    console.log(node.value);
+  /**
+   * Postorder traversal involves visiting the left subtree,
+   * followed by the right subtree, visiting the root node at the end.
+   * (left, right, root) order.
+   * @param {TreeNode<T> | null} node
+   * @returns {T[]}
+   */
+  protected postOrderTraversal(node: TreeNode<T> | null): T[] {
+    const values: T[] = [];
+    const postOrderHelper = function (node: TreeNode<T> | null, values: T[]) {
+      if (!node) return values;
+      postOrderHelper(node.left, values);
+      postOrderHelper(node.right, values);
+      values.push(node.value);
+
+      return values;
+    };
+
+    return postOrderHelper(node, values);
   }
 
-  traverse(): void {
-    this.preOrderTraversal(this.root);
-    console.log("");
-    this.inOrderTraversal(this.root);
-    console.log("");
-    this.postOrderTraversal(this.root);
+  /**
+   * Level order traversal involves traversing all the nodes
+   * at one level before proceeding to the next level.
+   * Also known as breadth first traversal.
+   * @param {TreeNode<T> | null} node
+   * @returns {T[]}
+   */
+  protected levelOrderTraversal(node: TreeNode<T> | null): T[] {
+    const values: T[] = [];
+
+    if (!node) return values;
+
+    // Initialize the queue with the root node for level order traversal (breadth first search)
+    const queue = new Queue([node]);
+    let poppedNode: TreeNode<T> | undefined;
+
+    while (!queue.isEmpty) {
+      poppedNode = queue.dequeue();
+      poppedNode && values.push(poppedNode.value);
+
+      // Add the left and right nodes to the queue.
+      poppedNode && poppedNode.left && queue.enqueue(poppedNode.left);
+      poppedNode && poppedNode.right && queue.enqueue(poppedNode.right);
+    }
+    return values;
   }
 
+  /**
+   * Traverse - Traverses the entire tree.
+   * @param {Traversal} traversal
+   * @returns {T[]}
+   */
+  traverse(traversal?: Traversal): T[] {
+    if (traversal === "PRE_ORDER") return this.preOrderTraversal(this.root);
+    else if (traversal === "POST_ORDER")
+      return this.postOrderTraversal(this.root);
+    else if (traversal === "LEVEL_ORDER")
+      return this.levelOrderTraversal(this.root);
+    return this.inOrderTraversal(this.root);
+  }
+
+  /**
+   * Calculates the height of the tree. The height a tree
+   * is the distance from the root node to its farthest leaf node.
+   * @param {TreeNode<T> | null} node
+   * @returns {number}
+   */
   protected calculateHeight(node: TreeNode<T> | null): number {
     if (!node) return 0;
     const leftHeight = this.calculateHeight(node.left);
